0bcc747a4013e892349a3f13e5d615eacf9f94c7
Ru Chern Chong <ruchern@sproud.biz>
2017-06-20 18:17:50 +0800

    Remove debugging stuff
    Signed-off-by: Ru Chern Chong <ruchern@sproud.biz>

--- Trippie/ViewController.swift
+++ Trippie/ViewController.swift
@@ -4,16 +4,14 @@ import UIKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     var mapView: GMSMapView?
-    var locationManager:CLLocationManager!
+    var locationManager: CLLocationManager!
     let marker = GMSMarker()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         mapView = GMSMapView()
-        
         locationManager = CLLocationManager()
-
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.distanceFilter = 50
@@ -28,22 +26,20 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if (status == CLAuthorizationStatus.authorizedWhenInUse) {
             mapView!.isMyLocationEnabled = true
         }
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let newLocation: CLLocationCoordinate2D = (manager.location?.coordinate)!
-        print(newLocation)
-        
+
         mapView!.camera = GMSCameraPosition.camera(withLatitude: newLocation.latitude, longitude: newLocation.longitude, zoom: 16.0)
         mapView!.settings.myLocationButton = true
         self.view = self.mapView!
-        
-        
+
         marker.position = CLLocationCoordinate2DMake(newLocation.latitude, newLocation.longitude)
         marker.map = self.mapView!
     }
