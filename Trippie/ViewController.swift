e16dbbeaf5b20a59d076c151994ba1f7b085005d
Ru Chern Chong <ruchern@sproud.biz>
2017-06-20 15:58:35 +0800

    Setup project
    Signed-off-by: Ru Chern Chong <ruchern@sproud.biz>

---
+++ Trippie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trippie
+//
+//  Created by Ru Chern Chong on 20/6/17.
+//  Copyright © 2017 Ru Chern Chong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
